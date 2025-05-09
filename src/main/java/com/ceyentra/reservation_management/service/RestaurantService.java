12f3150ac03ce6f75d480700a78ba7c6d3d4d58b
chulakasam <sampathchulaka33872@gmail.com>
2025-05-09 17:09:22 +0530

    restuarant entity list convert to dto and dto convert to entity

--- src/main/java/com/ceyentra/reservation_management/service/RestaurantService.java
+++ src/main/java/com/ceyentra/reservation_management/service/RestaurantService.java
@@ -2,6 +2,9 @@ package com.ceyentra.reservation_management.service;
 
 import com.ceyentra.reservation_management.dto.RestaurantDTO;
 
+import java.util.List;
+
 public interface RestaurantService {
     void saveRestaurant(RestaurantDTO  restaurantDTO);
+
 }
--- src/main/java/com/ceyentra/reservation_management/util/Mapping.java
+++ src/main/java/com/ceyentra/reservation_management/util/Mapping.java
@@ -3,8 +3,11 @@ package com.ceyentra.reservation_management.util;
 import com.ceyentra.reservation_management.dto.RestaurantDTO;
 import com.ceyentra.reservation_management.entity.Restaurant;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 public class Mapping {
@@ -18,5 +21,11 @@ public class Mapping {
     public RestaurantDTO toRestaurantDTO(Restaurant restaurantEntity){
         return modelMapper.map(restaurantEntity, RestaurantDTO.class);
     }
+    public List<RestaurantDTO> asRestaurantDTOList(List<Restaurant> restaurantList){return modelMapper.map(restaurantList, new TypeToken<List<RestaurantDTO>>(){}.getType());
+    }
+    public List<Restaurant> toRestaurantEntityList(List<RestaurantDTO> restaurantDTOList) {
+        return modelMapper.map(restaurantDTOList,new TypeToken<List<Restaurant>>() {}.getType());
+    }
+
 
 }
