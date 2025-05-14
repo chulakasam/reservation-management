44f1713582f5052705ad65fae4c2099a13a53aa5
chulakasam <sampathchulaka33872@gmail.com>
2025-05-14 13:08:13 +0530

    create reservation dto

--- src/main/java/com/ceyentra/reservation_management/dto/ReservationDTO.java
+++ src/main/java/com/ceyentra/reservation_management/dto/ReservationDTO.java
@@ -1,10 +1,26 @@
 package com.ceyentra.reservation_management.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Data
 public class ReservationDTO {
 
+    private int reservationId;
+    private int customerId;
+    private int restaurantId;
+    private int tableId;
+    private LocalDateTime reservationDateTime;
+    private int partySize;
+    private String status;
+    private String specialRequests;
+    private Integer userId;
+    private Timestamp creationDateTime;
+    private Timestamp lastModifiedDateTime;
 }
