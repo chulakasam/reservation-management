68923085579d1febabf2ef1bd5fe57e438f863d9
chulakasam <sampathchulaka33872@gmail.com>
2025-05-14 21:47:15 +0530

    create userdao class method

--- src/main/java/com/ceyentra/reservation_management/dao/UserDAO.java
+++ src/main/java/com/ceyentra/reservation_management/dao/UserDAO.java
@@ -4,6 +4,9 @@ import com.ceyentra.reservation_management.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserDAO extends JpaRepository<User,Integer> {
+    Optional<User> findByEmail(String email);
 }
