e7c771042ad19f0ec024a40f38bffdae30978ea5
chulakasam <sampathchulaka33872@gmail.com>
2025-05-14 22:08:57 +0530

    create usercontroller

--- src/main/java/com/ceyentra/reservation_management/controller/UserController.java
+++ src/main/java/com/ceyentra/reservation_management/controller/UserController.java
@@ -1,11 +1,34 @@
 package com.ceyentra.reservation_management.controller;
 
+import com.ceyentra.reservation_management.secure.Response.JWTAuthResponse;
+import com.ceyentra.reservation_management.secure.Security.SignIn;
+import com.ceyentra.reservation_management.secure.Security.SignUp;
+import com.ceyentra.reservation_management.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    public UserController(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+    @PostMapping("/signUp")
+    public ResponseEntity<JWTAuthResponse> saveUser(@RequestBody SignUp signUp) {
+        return ResponseEntity.ok(authenticationService.signUp(signUp));
+    }
+
+    @PostMapping("/signIn")
+    public ResponseEntity<JWTAuthResponse> signIn(@RequestBody SignIn signIn) {
+        return ResponseEntity.ok(authenticationService.signIn(signIn));
+    }
 
 
 }
